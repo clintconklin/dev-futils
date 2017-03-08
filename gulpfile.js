23f9d1670991725a66d4ea8a1036a4d15a5dd3ba
Clint Conklin <clint.conklin@gmail.com>
2017-03-08 07:36:15 -0800

    hirono tweak

--- gulpfile.js
+++ gulpfile.js
@@ -272,14 +272,14 @@ var defaults = {
         },
         "less": {
             "getDest": function(dir) {
-                if (dir.indexOf('themes/hirono') !== -1 || dir.indexOf('themes/vitter') !== -1 || dir.indexOf('themes/casey') !== -1 || dir.indexOf('themes/kaine') !== -1 || dir.indexOf('themes/donnelly') !== -1 || dir.indexOf('themes/murphy') !== -1 || dir.indexOf('themes/heinrich') !== -1) {
+                if (dir.indexOf('themes/hirono_v1') !== -1 || dir.indexOf('themes/vitter') !== -1 || dir.indexOf('themes/casey') !== -1 || dir.indexOf('themes/kaine') !== -1 || dir.indexOf('themes/donnelly') !== -1 || dir.indexOf('themes/murphy') !== -1 || dir.indexOf('themes/heinrich') !== -1) {
                     return dir.replace(/\/amend/i, '') + '../';
                 } else {
                     return dir + '../';
                 }
             },
             "getTarget": function(dir, file) {
-                if (dir.indexOf('themes/hirono') !== -1 || dir.indexOf('themes/vitter') !== -1 || dir.indexOf('themes/casey') !== -1 || dir.indexOf('themes/kaine') !== -1 || dir.indexOf('themes/donnelly') !== -1 || dir.indexOf('themes/murphy') !== -1 || dir.indexOf('themes/heinrich') !== -1) { // target file is bootstrap.less, and some less files are in an /amend subdirectory
+                if (dir.indexOf('themes/hirono_v1') !== -1 || dir.indexOf('themes/vitter') !== -1 || dir.indexOf('themes/casey') !== -1 || dir.indexOf('themes/kaine') !== -1 || dir.indexOf('themes/donnelly') !== -1 || dir.indexOf('themes/murphy') !== -1 || dir.indexOf('themes/heinrich') !== -1) { // target file is bootstrap.less, and some less files are in an /amend subdirectory
 					return dir.replace(/\/amend/i, '') + 'bootstrap.less';
                 } else {
                     return dir + 'common.less';
